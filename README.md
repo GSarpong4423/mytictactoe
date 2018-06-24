cae08b662aac801ebc05eda6cb97d4adcc397bb2
GSarpong4423 <sarpongg@gmail.com>
2018-06-23 22:42:43 -0400

    Created grid, click event function

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Tic Tac Toe
+# Tic Tac Toe
+
+# User Stories
+* User enter url into browser
+* User can create an account
+* User can sign into the game
+* User play you game
+# as <user> -  see stats
+                       Quit game
+                       Play new game 
+                       Change passw
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,25 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const PLAYER_TOKEN = 'X'
+const COMPUTER_TOKEN = 'O'
 
 $(() => {
-  // your JS code goes here
+  const grid = [
+    ['','','',''],
+    ['','','',''],
+    ['','','',''] // blank 3x3 grid array and open strings
+  ];
+  // event for user to click
+  $('.col').click(function() {
+    // $this = $(this);
+    $(this).html(PLAYER_TOKEN);
+    const r = $(this).data('r');  // .this is equal to 'r' and 'c'
+    const c = $(this).data('c');
+    grid[r][c] = PLAYER_TOKEN;
+    console.log(grid);
+  });
 })
+
+
+// not to repeat code
